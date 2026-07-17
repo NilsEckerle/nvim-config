22c8158e7d004d98086d7ff5e2ca15983ca3f4d3
Nils Eckerle <nilseckerle2001@protonmail.com>
2026-07-17 20:02:22 +0200

    style(render-markdown): reformat with tabs

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,29 +1,32 @@
 return {
-  "MeanderingProgrammer/render-markdown.nvim",
-  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-  ---@module 'render-markdown'
-  ---@type render.md.UserConfig
-  opts = {
-    filetype = { "markdown", "markdown_inline" },
-    heading = {
-      backgrounds = {
-        'RenderMarkdownH3Bg',
-        'RenderMarkdownH3Bg',
-        'RenderMarkdownH3Bg',
-        'RenderMarkdownH3Bg',
-        'RenderMarkdownH3Bg',
-        'RenderMarkdownH3Bg',
-      },
-      -- Highlight for the heading and sign icons.
-      -- Output is evaluated using the same logic as 'backgrounds'.
-      foregrounds = {
-        'RenderMarkdownH1',
-        'RenderMarkdownH2',
-        'RenderMarkdownH3',
-        'RenderMarkdownH4',
-        'RenderMarkdownH5',
-        'RenderMarkdownH6',
-      },
-    },
-  },
+	"MeanderingProgrammer/render-markdown.nvim",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-tree/nvim-web-devicons",
+	},
+	---@module 'render-markdown'
+	---@type render.md.UserConfig
+	opts = {
+		filetype = { "markdown", "markdown_inline" },
+		heading = {
+			backgrounds = {
+				"RenderMarkdownH3Bg",
+				"RenderMarkdownH3Bg",
+				"RenderMarkdownH3Bg",
+				"RenderMarkdownH3Bg",
+				"RenderMarkdownH3Bg",
+				"RenderMarkdownH3Bg",
+			},
+			-- Highlight for the heading and sign icons.
+			-- Output is evaluated using the same logic as 'backgrounds'.
+			foregrounds = {
+				"RenderMarkdownH1",
+				"RenderMarkdownH2",
+				"RenderMarkdownH3",
+				"RenderMarkdownH4",
+				"RenderMarkdownH5",
+				"RenderMarkdownH6",
+			},
+		},
+	},
 }
